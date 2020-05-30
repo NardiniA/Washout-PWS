e530c60f07bafd83b01a911bb74a2c91b0cc8016
= <techteacher.pershore@gmail.com>
2020-05-30 21:03:06 +0100

    Updated Contact Form php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -23,7 +23,7 @@ $services = strip_tags(htmlspecialchars($_POST['selectService']));
 $to = 'info@washoutpressurewashing.co.uk'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nAddress: $address\n\nService Selected: $selectService\n\nMessage:\n$message";
-$headers = "From: no.reply.washout@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: emailtesting531@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 $mail = mail($to,$email_subject,$email_body,$headers);
 if ($mail == true) {
